10b09fe52d5e53fc9518ad48da3b8e0231261545
Alberto Pérez <abrecht3@gmail.com>
2016-03-21 18:24:07 -0600

    Initial commit

---
+++ app/src/main/java/com/example/albertoperez/changecolorexample/Datos.java
@@ -0,0 +1,18 @@
+package com.example.albertoperez.changecolorexample;
+
+/**
+ * Created by Alberto Perez on 10/09/2015.
+ */
+public class Datos {
+    public int icon;
+    public  String nombre;
+
+    public Datos(){
+        super();
+    }
+    public Datos(int icon,String nombre){
+        super();
+        this.icon=icon;
+        this.nombre=nombre;
+    }
+}
